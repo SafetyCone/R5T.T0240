5075c609c52dacef6a06a56ff51b895f4f1ba646
davidcoats <david@getsafetycone.com>
2024-01-20 14:29:15 -0800

    Initial commit.

---
+++ source/R5T.T0240/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0240
+{
+	/// <summary>
+	/// IHasX and IWithX marker attribute and marker interface types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0240/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0240
+IHasX and IWithX marker attribute and marker interface types library.
